ef5ab0814495f6d4849f6eefd14966e34cae6a3b
johannpa <johann_paque@hotmail.fr>
2023-10-01 20:06:00 +0200

    Just read the csv file

--- CsvTestDotNet/PersonModel.cs
+++ CsvTestDotNet/PersonModel.cs
@@ -13,27 +13,27 @@ namespace CsvTestDotNet
         public int Index { get; set; }
 
         [Name("User Id")]
-        public string UserId { get; set; }
+        public string? UserId { get; set; }
 
         [Name("First Name")]
-        public string FirstName { get; set; }
+        public string? FirstName { get; set; }
 
         [Name("Last Name")]
-        public string LastName { get; set; }
+        public string? LastName { get; set; }
 
         [Name("Sex")]
-        public string Sex { get; set; }
+        public string? Sex { get; set; }
 
         [Name("Email")]
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
         [Name("Phone")]
-        public string Phone { get; set; }
+        public string? Phone { get; set; }
 
         [Name("Date of birth")]
         public DateTime DateOfBirth { get; set; }
 
         [Name("Job Title")]
-        public string JobTitle { get; set; }
+        public string? JobTitle { get; set; }
     }
 }
--- CsvTestDotNet/Program.cs
+++ CsvTestDotNet/Program.cs
@@ -1,5 +1,23 @@
 ﻿// CSV for Comma Separated Values
 // 1, Mike, Tysan, 555-1313
 
+using CsvHelper;
+using CsvTestDotNet;
+using System.Globalization;
+
 string inputFile = @"C:\CSVFileTest\people-100.csv";
 string outputFile = @"C:\CSVFileTest\filtered-people.csv";
+
+List<PersonModel> outputRecords = new();
+
+using var reader = new StreamReader(inputFile);
+using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+var records = csv.GetRecords<PersonModel>();
+
+foreach (var record in records)
+{
+    
+   Console.WriteLine(record.FirstName + " " + record.LastName);
+    
+}
