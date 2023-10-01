8514836db7a10320db4dcb357b9b48640e87a9c1
johannpa <johann_paque@hotmail.fr>
2023-10-01 19:52:18 +0200

    Add package nuget CSVHelper and create model for the csv file

--- CsvTestDotNet/Program.cs
+++ CsvTestDotNet/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿// CSV for Comma Separated Values
+// 1, Mike, Tysan, 555-1313
+
+string inputFile = @"C:\CSVFileTest\people-100.csv";
+string outputFile = @"C:\CSVFileTest\filtered-people.csv";
